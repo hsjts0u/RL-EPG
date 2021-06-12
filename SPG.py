b7847aba248217bd5bcb681f11dede7778d9c2a0
Jay Tsou <hsjts0u@gmail.com>
2021-06-12 22:06:16 +0800

    SPG-fix

--- SPG.py
+++ SPG.py
@@ -10,27 +10,12 @@ from itertools import count
 
 from model import (Actor, Critic)
 
-USE_CUDA = torch.cuda.is_available()
-
 LR = 0.001
 SIGMA = 0.2
 BATCH_SIZE = 64
 DISCOUNT = 0.99
 
-MAX_EPISODE = 20
-
-loss = nn.MSELoss()
-
-def to_numpy(var):
-    """
-    turn pytorch tensor to numpy array
-    """
-    return var.cpu().data.numpy() if USE_CUDA else var.data.numpy()
-
-def to_tensor(ndarray, requires_grad=False, dtype=torch.float32):
-    """ turn numpy array to pytorch tensor  """
-    return torch.tensor(torch.from_numpy(ndarray),
-                        dtype=dtype, requires_grad=requires_grad)
+MAX_EPISODE = 1000
 
 
 class SPG(object):
@@ -47,47 +32,41 @@ class SPG(object):
         self.batch_size = BATCH_SIZE
         self.discount = DISCOUNT
 
-        if USE_CUDA:
-            self.cuda()
-    
-    def cuda(self):
-        self.actor.cuda()
-        self.critic.cuda()
-    
-    def train(self, num_iter, env, max_episode_length=None):
+    def train(self, env):
         for episode in range(MAX_EPISODE):
             state = env.reset()
             episode_score = 0
             s_a = []
             
             for episode_steps in count():
-                mean = self.actor(to_tensor(state))
+                mean = self.actor(torch.Tensor(state))
                 action = torch.clip(
                             torch.normal(mean, SIGMA),
                             float(env.action_space.low[0]), 
                             float(env.action_space.high[0]))
-                next_state, reward, done, _ = env.step(to_numpy(action))
+                next_state, reward, done, _ = env.step(action.data.numpy())
                 episode_score += reward 
                 
-                s_a.append((state, action, reward, mean))
+                s_a.append((state, action, reward, mean, done))
                 state = next_state
                 
                 if len(s_a) > self.batch_size or done:
                     self.update_actor(s_a)
                     self.update_critic(s_a)
                     s_a.clear()
-                
+
                 if done:
-                    print(f"{episode}: episode_score is {episode_score}")
+                    print(f"{episode}: episode_score is {episode_score}, episode_length is {episode_steps}")
                     break
                     
     def update_actor(self, s_a):
         gamma_accum = 1
         loss = 0
-        for (state, action, reward, mean) in s_a:
+        for (state, action, reward, mean, done) in s_a:
             action_dist = torch.distributions.normal.Normal(mean, 0.2)
             sa_cat = torch.cat((torch.Tensor(state), action), dim=-1)
-            loss += gamma_accum * self.critic(sa_cat).detach_() * \
+            if not done:
+                loss += gamma_accum * self.critic(sa_cat).detach_() * \
                         -action_dist.log_prob(action)
             gamma_accum *= self.discount
         
@@ -101,8 +80,11 @@ class SPG(object):
         for i in range(len(s_a) - 1):
             sa_cat = torch.cat((torch.Tensor(s_a[i][0]), s_a[i][1].detach()), dim=-1)
             sa_n_cat = torch.cat((torch.Tensor(s_a[i+1][0]), s_a[i+1][1].detach()), dim=-1)
-            target = s_a[i][2] + self.discount * self.critic(sa_n_cat).detach_()
-            loss += F.mse_loss(self.critic(sa_cat), target)
+            if not s_a[i+1][4]:
+                target = s_a[i][2] + self.discount * self.critic(sa_n_cat).detach_()
+            else:
+                target = s_a[i][2]
+            loss += F.mse_loss(self.critic(sa_cat), torch.Tensor([target]))
             
         self.critic_optim.zero_grad()
         loss.backward()
@@ -111,8 +93,10 @@ class SPG(object):
                 
 if __name__ == "__main__":
     env = gym.make("MountainCarContinuous-v0")
+    env.seed(20)
+    torch.manual_seed(20)
     model = SPG(env.observation_space.shape[0], env.action_space.shape[0])
-    model.train(20, env, 280000)
+    model.train(env)
                 
                 
                 
