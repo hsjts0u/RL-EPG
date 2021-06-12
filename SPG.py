b65cbbdae5a206852a7f772a286b916687511d17
Jay Tsou <hsjts0u@gmail.com>
2021-06-12 18:21:16 +0800

    SPG-check

--- SPG.py
+++ SPG.py
@@ -1,12 +1,14 @@
 # SPG
 
+import gym
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.optim import Adam
 from itertools import count
 
-from .model import (Actor, Critic)
+from model import (Actor, Critic)
 
 USE_CUDA = torch.cuda.is_available()
 
@@ -27,23 +29,8 @@ def to_numpy(var):
 
 def to_tensor(ndarray, requires_grad=False, dtype=torch.float32):
     """ turn numpy array to pytorch tensor  """
-    return torch.tensor(torch.from_numpy(ndarray=ndarray),
+    return torch.tensor(torch.from_numpy(ndarray),
                         dtype=dtype, requires_grad=requires_grad)
-
-class Memory(object):
-    def __init__(self, memory_size=10000):
-        self.memory = deque(maxlen=memory_size)
-        self.memory_size = memory_size
-
-    def __len__(self):
-        return len(self.memory)
-
-    def append(self, item):
-        self.memory.append(item)
-
-    def sample_batch(self, batch_size):
-        idx = np.random.permutation(len(self.memory))[:batch_size]
-        return [self.memory[i] for i in idx]
 
 
 class SPG(object):
@@ -56,8 +43,6 @@ class SPG(object):
 
         self.critic = Critic(self.nb_states, self.nb_actions)
         self.critic_optim = Adam(self.critic.parameters(), lr=LR)
-        
-        self.memory = Memory(10000)
 
         self.batch_size = BATCH_SIZE
         self.discount = DISCOUNT
@@ -69,59 +54,65 @@ class SPG(object):
         self.actor.cuda()
         self.critic.cuda()
     
-    def train(self, num_iter, env, output, max_episode_length=None):
+    def train(self, num_iter, env, max_episode_length=None):
         for episode in range(MAX_EPISODE):
             state = env.reset()
             episode_score = 0
             s_a = []
             
             for episode_steps in count():
+                mean = self.actor(to_tensor(state))
                 action = torch.clip(
-                            torch.normal(
-                                self.actor(to_tensor(state)), SIGMA
-                            ),
+                            torch.normal(mean, SIGMA),
                             float(env.action_space.low[0]), 
                             float(env.action_space.high[0]))
                 next_state, reward, done, _ = env.step(to_numpy(action))
                 episode_score += reward 
                 
-                s_a.append((state, action, reward))
+                s_a.append((state, action, reward, mean))
                 state = next_state
                 
                 if len(s_a) > self.batch_size or done:
-                    update_actor(s_a)
-                    update_critic(s_a)
+                    self.update_actor(s_a)
+                    self.update_critic(s_a)
                     s_a.clear()
+                
+                if done:
+                    print(f"{episode}: episode_score is {episode_score}")
+                    break
                     
-    def update_actor(s_a):
+    def update_actor(self, s_a):
         gamma_accum = 1
         loss = 0
-        for (state, action, reward) in s_a:
-            action_dist = torch.distributions.normal.Normal(to_tensor(action), 0.2)
-            sa_cat = torch.cat((to_tensor(state), to_tensor(action)), dim=1)
-            loss += gamma_accum * self.critic(sa_cat) * \
-                        -action_dist.log_prob(to_tensor(action))
+        for (state, action, reward, mean) in s_a:
+            action_dist = torch.distributions.normal.Normal(mean, 0.2)
+            sa_cat = torch.cat((torch.Tensor(state), action), dim=-1)
+            loss += gamma_accum * self.critic(sa_cat).detach_() * \
+                        -action_dist.log_prob(action)
             gamma_accum *= self.discount
         
+        loss = loss.mean()
         self.actor_optim.zero_grad()
         loss.backward()
         self.actor_optim.step()
     
-    def update_critic(s_a):
+    def update_critic(self, s_a):
         loss = 0
-        for i in (len(s_a) - 1):
-            sa_cat = torch.cat((to_tensor(s_a[i].state), to_tensor(s_a[i].action)), dim=1)
-            sa_n_cat = torch.cat((to_tensor(s_a[i+1].state), to_tensor(s_a[i+1].action)), dim=1)
-            target = r + self.discount * self.critic(sa_n_cat)
+        for i in range(len(s_a) - 1):
+            sa_cat = torch.cat((torch.Tensor(s_a[i][0]), s_a[i][1].detach()), dim=-1)
+            sa_n_cat = torch.cat((torch.Tensor(s_a[i+1][0]), s_a[i+1][1].detach()), dim=-1)
+            target = s_a[i][2] + self.discount * self.critic(sa_n_cat).detach_()
             loss += F.mse_loss(self.critic(sa_cat), target)
-                
+            
         self.critic_optim.zero_grad()
         loss.backward()
         self.critic_optim.step()
                 
                 
-                
-                
+if __name__ == "__main__":
+    env = gym.make("HalfCheetah-v2")
+    model = SPG(env.observation_space.shape[0], env.action_space.shape[0])
+    model.train(20, env, 280000)
                 
                 
                 
