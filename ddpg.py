fea69d399806d2faa8b277a5a4fb9d5c9201cacb
psychicalcoder <psychicalcoder@gmail.com>
2021-06-14 17:35:30 +0800

    test inverted pendulum, add action scale

--- ddpg.py
+++ ddpg.py
@@ -18,6 +18,7 @@ USE_CUDA = torch.cuda.is_available()
 
 LR = 0.001
 RMSIZE = 100000
+INGORE_STEP = 10000
 WINDOW_LEN = 1
 TAU = 0.01
 OU_PSI = 0.15
@@ -42,7 +43,7 @@ def soft_update(target, source, tau):
     """
     for target_param, param in zip(target.parameters(), source.parameters()):
         target_param.data.copy_(
-            target.param.data * (1.0 - tau) + param.data * tau
+            target_param.data * (1.0 - tau) + param.data * tau
         )
 
 def to_numpy(var):
@@ -57,9 +58,11 @@ def to_tensor(ndarray, requires_grad=False, dtype=torch.float32):
                         dtype=dtype, requires_grad=requires_grad)
         
 class DDPG:
-    def __init__(self, nb_states, nb_actions):
+    def __init__(self, nb_states, nb_actions, action_high, action_low):
         self.nb_states = nb_states
         self.nb_actions = nb_actions
+        self.action_high = action_high
+        self.action_low = action_low
 
         self.actor = Actor(self.nb_states, self.nb_actions)
         self.actor_target = Actor(self.nb_states, self.nb_actions)
@@ -68,14 +71,15 @@ class DDPG:
         self.critic = Critic(self.nb_states, self.nb_actions)
         self.critic_target = Critic(self.nb_states, self.nb_actions)
         self.critic_optim = Adam(self.critic.parameters(), lr=LR)
-
+        # self.load_weights("ddpg_ip")
+        
         hard_update(self.actor_target, self.actor)
         hard_update(self.critic_target, self.critic)
 
         self.batch_size = BATCH_SIZE
         self.discount = DISCOUNT
         self.tau = TAU
-        self.ignore_step = 10000
+        self.ignore_step = INGORE_STEP
         self.memory = SequentialMemory(limit=RMSIZE, window_length=WINDOW_LEN)
         self.random_process = OUProcess(size=self.nb_actions, theta=OU_PSI,
                                         mu=0.0, sigma=OU_SIGMA)
@@ -106,7 +110,9 @@ class DDPG:
 
     def select_action(self, s_t):
         action = to_numpy(self.actor(to_tensor(np.array([s_t])))).squeeze(0)
+        action = action*self.action_high
         action += self.is_training * self.random_process.sample()
+        action = np.clip(action, self.action_low, self.action_high)
         self.a_t = action
         return action
 
@@ -140,6 +146,7 @@ class DDPG:
         state_batch, action_batch, reward_batch, next_state_batch, \
             terminal_batch = self.memory.sample_and_split(self.batch_size)
 
+        # print(state_batch.shape)
         next_q_values = self.critic_target(
             torch.cat((
                 to_tensor(next_state_batch),
@@ -166,6 +173,7 @@ class DDPG:
 
         policy_loss = policy_loss.mean()
         policy_loss.backward()
+        # print("{vl} {pl}".format(vl=value_loss.item(), pl=policy_loss.item()))
         self.actor_optim.step()
         self.actor_optim.zero_grad()
 
@@ -178,7 +186,7 @@ class DDPG:
         episode_steps = 0
         episode_reward = 0
         observation = None
-        while step < num_iter:
+        while episode < num_iter:
             if observation is None:
                 observation = deepcopy(env.reset())
                 self.reset(observation)
@@ -210,6 +218,9 @@ class DDPG:
                     False
                 )
 
+                if episode % 100 == 0:
+                    print(f"step {step} episode {episode} got {episode_reward} rewards in {episode_steps}")
+                
                 observation = None
                 episode_reward = 0.0
                 episode_steps = 0
@@ -221,7 +232,8 @@ if __name__ == '__main__':
 
     env = gym.make(env_list[1])
     env.seed(20)
-
-    ddpg = DDPG(env.observation_space.shape[0], env.action_space.shape[0])
-    ddpg.train(10, env, "IPv2", max_episode_length=1000)
+    ddpg = DDPG(env.observation_space.shape[0], env.action_space.shape[0],
+                env.action_space.high[0], env.action_space.low[0])
+    # ddpg.load_weights("ddpg_ip")
+    ddpg.train(10000, env, "ddpg_ip", max_episode_length=1000)
     print("done")
