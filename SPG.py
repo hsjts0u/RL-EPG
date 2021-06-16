784f07cb5304483e9593cfd7fd4537a265752871
Jay Tsou <hsjts0u@gmail.com>
2021-06-16 18:55:24 +0800

    SPG-actor_critic_v

--- SPG.py
+++ SPG.py
@@ -12,57 +12,61 @@ from torch.distributions.normal import Normal
 from copy import deepcopy
  
 from model import (Actor, Critic)
- 
-torch.autograd.set_detect_anomaly(True)
+from graphing import learning_curve
  
 class SPG(object):
     def __init__(self, actor_lr, critic_lr, gamma, nb_states, nb_actions,
-                 max_episodes, max_ep_steps, reward_scaling):
+                 max_episodes, max_ep_steps, reward_scaling, max_steps):
         
         self.gamma = gamma
         self.max_episodes = max_episodes
         self.max_ep_steps = max_ep_steps
         self.reward_scaling = reward_scaling
+        self.max_steps = max_steps
         
         self.actor = Actor(nb_states, nb_actions)
         self.actor_optim = Adam(self.actor.parameters(), lr=actor_lr)
         
-        self.critic = Critic(nb_states, nb_actions)
+        self.critic = Critic(nb_states, 0)
         self.critic_optim = Adam(self.critic.parameters(), lr=critic_lr)
         
         
     def train(self, env):
         ewma_rewards = 0.0
         step_count = 0
+        graph_data = []
         
         for eps in range(self.max_episodes):
+            
+            if step_count >= self.max_steps:
+                break
+            
             eps_score = 0
             state = env.reset()
-            action, log_prob = self.choose_action(state, env)
             
             for step in range(self.max_ep_steps):
                 step_count += 1
-                
+                action, log_prob = self.choose_action(state, env)
                 next_state, reward, done, _ = env.step(action)
-                next_action, next_log_prob = self.choose_action(next_state, env)
                 
                 eps_score += reward
                 reward *= self.reward_scaling
                 
-                self.learn(state, action, reward, next_state, \
-                    next_action, log_prob, done)
+                self.learn(state, action, reward, next_state, log_prob, done)
  
                 state = next_state
-                action = next_action
-                log_prob = next_log_prob
  
                 if done:
+                    graph_data.append((step_count, eps_score))
                     ewma_rewards = 0.05 * eps_score + 0.95 * ewma_rewards
+                    
                     if (eps % 100) == 0:
                         print('Steps {}\tEpisode {}\tlength: {}\tewma_reward: {}'.format(\
                             step_count, eps, step+1, ewma_rewards))
                     break
- 
+            
+        learning_curve([graph_data], "SPG")
+        
         
     def choose_action(self, state, env):
         mean = self.actor(torch.Tensor(state)) * env.action_space.high[0]
@@ -76,17 +80,15 @@ class SPG(object):
         return action.detach().numpy(), log_prob
            
            
-    def learn(self, state, action, reward, next_state, next_action, log_prob, done):
-        qa = torch.cat((torch.Tensor(state), \
-            torch.Tensor(action)), dim=-1)
-        nqa = torch.cat((torch.Tensor(next_state), \
-            torch.Tensor(next_action)), dim=-1)
+    def learn(self, state, action, reward, next_state, log_prob, done):
+        s = torch.Tensor(state)
+        ns = torch.Tensor(next_state)
  
-        q = self.critic(qa)
-        next_q = self.critic(nqa)
+        v = self.critic(s)
+        nv = self.critic(ns)
         
-        actor_loss = self.gamma * q.detach() * -log_prob
-        critic_loss = F.mse_loss(reward + self.gamma * next_q.detach() * (1-int(done)), q)
+        actor_loss = (reward + self.gamma * nv.detach() - v.detach())* -log_prob
+        critic_loss = F.mse_loss(reward + self.gamma * nv.detach() * (1-int(done)), v)
         
         self.actor_optim.zero_grad()
         self.critic_optim.zero_grad()
@@ -107,7 +109,7 @@ if __name__ == "__main__":
     env.seed(20)
     #print(float(env.action_space.low[0]), float(env.action_space.high[0]))
     model = SPG(0.001, 0.001, 0.9, env.observation_space.shape[0],\
-        env.action_space.shape[0], 100_000, 1000, envs[0][1])
+        env.action_space.shape[0], 100_000, 1000, envs[0][1], 100_000)
     model.train(env)
     
     
